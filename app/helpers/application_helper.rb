fee8f7d6601d5b861af12c2c928c1f8952ea5710
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-05-10 18:54:26 -0300

    Helper para colocar nos selects. [@jaodsilv]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+	def sexos
+		['Marculino', 'Feminino']
+	end
+
+	def estados_civis
+		['Solteiro', 'Casado', 'Divorciado', 'Viuvo']
+	end
+
+	def estados
+		['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE',
+			'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+	end
+
+	def documentos
+		['RG', 'RNE']
+	end
+
+	def cores
+		['Branca', 'Parda', 'Preta', 'Amarela', 'Indigena', 'Desconhecida']
+	end
+
+	def categorias_idade
+		['Maior', 'Menor']
+	end
+
+	def sim_nao
+		[['sim', true], ['não', false]]
+	end
 end
