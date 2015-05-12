4c2fb8747a0b1c6f51a4c029a8d19c44c6b23c31
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-05-12 00:33:20 -0300

    Escolha de produtos view. [@jaodsilv]

--- app/helpers/produtos_helper.rb
+++ app/helpers/produtos_helper.rb
@@ -1,2 +1,11 @@
 module ProdutosHelper
+	def dimensoes_urnas
+		# Deveria pegar essa informação do banco de dados da loja
+		["2,00x0,60x0,40", "1,90x0,60x0,40"]
+	end
+
+	def dimensoes_revestimentos
+		# Deveria pegar essa informação do banco de dados da loja
+		["2,00x0,60x0,40", "1,90x0,60x0,40"]
+	end
 end
