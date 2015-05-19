a82ed6837e979527e81758c1bd4e407a4bc157ef
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2015-05-19 17:49:18 -0300

    Validações quase feitas, falta verificar se as relações has_one e belongs_to estão certas. Falta fazer algo para ele voltar para a página que tem erro, porque por enquanto ele continua para o próximo formulário mas não salva no banco.
    [@yaragg, @karinaawoki]

--- app/models/contratante.rb
+++ app/models/contratante.rb
@@ -2,5 +2,16 @@ class Contratante < ActiveRecord::Base
   has_one :localizacao
   belongs_to :cadastro
 
-  accepts_nested_attributes_for :localizacao
+  accepts_nested_attributes_for :localizacao  
+  validates_associated :localizacao
+  
+  validates :nome_completo, presence:true, length: {maximum: 50}, numericality: false
+  validates :cpf, presence: true
+  validates :rg, presence: true
+  validates :profissao, numericality: false, length: {maximum: 50}
+  validates :parentesco, numericality: false, length: {maximum: 50}
+  validates :email, length: {maximum: 50}
+  validates :nome_mae, presence:true, length: {maximum: 50}, numericality: false
+  validates :cnpj, length: {maximum: 50}
+  validates :nome_empresarial, length: {maximum: 50}
 end
