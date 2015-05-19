423046d4fe1da5f0d43fa4d92eb9a0b82bbc5592
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2015-05-19 17:49:18 -0300

    Validações quase feitas, falta verificar se as relações has_one e belongs_to estão certas. Falta fazer algo para ele voltar para a página que tem erro, porque por enquanto ele continua para o próximo formulário mas não salva no banco.
    [@yaragg, @karinaawoki]

--- app/models/contratante.rb
+++ app/models/contratante.rb
@@ -1,4 +1,17 @@
 class Contratante < ActiveRecord::Base
   belongs_to :localizacao
   has_one :cadastro
+  
+  validates_associated :localizacao
+  
+  validates :nome_completo, presence:true, length: {maximum: 50}, numericality: false
+  validates :cpf, presence: true
+  validates :rg, presence: true
+  validates :profissao, numericality: false, length: {maximum: 50}
+  validates :parentesco, numericality: false, length: {maximum: 50}
+  validates :email, length: {maximum: 50}
+  validates :nome_mae, presence:true, length: {maximum: 50}, numericality: false
+  validates :cnpj, length: {maximum: 50}
+  validates :nome_empresarial, length: {maximum: 50}
+    
 end
--- app/models/localizacao.rb
+++ app/models/localizacao.rb
@@ -1,4 +1,13 @@
 class Localizacao < ActiveRecord::Base
 	has_one :falecido
 	has_one :contratante
+	
+	validates :endereco, presence: true, length: {maximum: 50}
+	validates :bairro, presence: true, length: {maximum: 50}
+	validates :numero, presence: true, length:{maximum: 10}, numericality: {only_integer: true}
+	validates :cidade, presence: true, length:{maximum: 50}, numericality: false
+	validates :complemento, length: {maximum: 50}
+	validates :cep, presence: true, length:{maximum: 10}
+	validates :estado, presence: true, length: {maximum: 2}, numericality: false
+	
 end
