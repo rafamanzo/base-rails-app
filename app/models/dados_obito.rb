c832de291d8c7e16aa0d67c70302f87968d05567
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2015-05-20 15:27:16 -0300

    Validação dos atributos de DadosObito.
    [@yaragg, @jackiewalker]

--- app/models/dados_obito.rb
+++ app/models/dados_obito.rb
@@ -1,3 +1,9 @@
 class DadosObito < ActiveRecord::Base
 	has_one :cadastro
+	
+	validates :local_falecimento, length: {maximum: 50}, numericality: false
+	validates :cartorio, :causa_mortis, :cemiterio, :endereco, :medico, :proaim, length: {maximum: 50}, numericality: false
+	validates :crm, length: {maximum : 10}, numericality: {only_integer: true}
+	#Não fizemos validação das datas e horas de falecimento e sepultamento, se não der erro para valores inválidos teremos que fazer a validação
+	validates :observacoes, length: {maximum: 1023}
 end
