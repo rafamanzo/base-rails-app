7c9e628af0afdc17a99ee337db409e2c8a0b8c16
felipedr <felipedrios@hotmail.com>
2015-05-27 15:15:10 -0300

    Inicio de testes js com konacha [@felipedr,@suelengc]

---
+++ app/assets/javascripts/teste.js
@@ -0,0 +1,8 @@
+
+
+function testa_a_porra_do_js(){
+	var teste = 1;
+
+	return teste;
+
+}
---
+++ spec/javascripts/application_spec.js
@@ -0,0 +1,36 @@
+//= require application
+
+describe("cpf", function() {
+  it("testando mascara de cpf", function() {
+    cpf_mask('41884916805').should.equal('418.849.168-05');
+  });
+
+});
+
+describe("telefone", function() {
+  it("testando mascara telefone", function() {
+    telefone_mask('11912345678').should.equal('(11) 91234-5678');
+  });
+
+});
+
+describe("hora", function() {
+  it("testando mascara hora", function() {
+    hora_mask('1232').should.equal('12:32');
+  });
+
+  it("testando mascara hora com 1 digito", function() {
+    hora_mask('123').should.equal('1:23');
+  });
+
+});
+
+describe("cep", function() {
+  it("testando mascara cep", function() {
+    cep_mask('12345678').should.equal('12345-678');
+  });
+
+
+});
+
+
