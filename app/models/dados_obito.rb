a995418a7326b26d38d22e5d2db527380a2e83a5
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-06-10 15:48:52 -0300

    Validações em português. [@karinaawoki, @jackiewalker, @jaodsilv]

--- app/models/dados_obito.rb
+++ app/models/dados_obito.rb
@@ -5,23 +5,23 @@ class DadosObito < ActiveRecord::Base
   accepts_nested_attributes_for :dados_velorio
   # validates_associated :dados_velorio
 	# accepts_nested_attributes_for :corpo_velado
-	# validates_associated :corpo_a_ser_velado, if: :PNS?
+  validates_associated :dados_velorio, if: :PNS?
 	# TODO criar esse dropdown
 
 	#Geral
-	validates :local_falecimento, :causa_mortis, :endereco, :medico, length: {maximum: 255}, numericality: false
-	validates :cartorio, :cemiterio, length: {maximum: 50}
-	validates :proaim, length: {maximum: 20}
-	validates :crm, length: {maximum: 10}, numericality: {only_integer: true}
-	validates :observacoes, length: {maximum: 1023}
+	validates :local_falecimento, :causa_mortis, :endereco, :medico, length: {maximum: 255, message: "pode ter no máximo 255 caracteres."}, numericality: false
+	validates :cartorio, :cemiterio, length: {maximum: 50, message: "pode ter no máximo 50 caracteres."}
+	validates :proaim, length: {maximum: 20, message: "pode ter no máximo 20 caracteres."}
+	validates :crm, length: {maximum: 10, message: "pode ter no máximo 10 caracteres."}, numericality: {only_integer: true, message: "só pode conter numeros."}
+	validates :observacoes, length: {maximum: 1023, message: "pode ter no máximo 1023 caracteres."}
 
 	#GD
 	# Perguntar sobre obrigatoriedades pro cliente.
 	#Não fizemos validação das datas e horas de falecimento e sepultamento, se não der erro para valores inválidos teremos que fazer a validação
 
 	#PNS
-	validates :endereco, length: {maximum: 255}, numericality: false, if: :PNS?
-	validates :cartorio, :causa_mortis, :cemiterio, :medico, :proaim, :local_falecimento, :crm, presence: true, if: :PNS?
+	validates :endereco, length: {maximum: 255, message: "pode ter no máximo 255 caracteres."}, numericality: false, if: :PNS?
+	validates :cartorio, :causa_mortis, :cemiterio, :medico, :proaim, :local_falecimento, :crm, presence: { message: "não pode ser vazio."}, if: :PNS?
 	#Não fizemos validação das datas e horas de falecimento e sepultamento, se não der erro para valores inválidos teremos que fazer a validação
 
 
--- app/models/dados_velorio.rb
+++ app/models/dados_velorio.rb
@@ -1,12 +1,17 @@
 class DadosVelorio < ActiveRecord::Base
   belongs_to :dados_obito
 
-  # validates :local_velorio, presence: true, length: {maximum: 255}, numericality: false
-  # validates :endereco, presence: true, length: {maximum: 255}, numericality: false
-  # validates :data_saida, presence: true
-  # validates :hora_saida, presence: true, length: {maximum: 5}
-  # validates :crematorio, presence: true, length: {maximum: 255}, numericality: false
-  # validates :endereco_crematorio, presence: true, length: {maximum: 255}, numericality: false
-  # validates :data_cremacao, presence: true
-  # validates :hora_cremacao, presence: true, length: {maximum: 5}
+  validates :local_velorio, :endereco, presence: { message: "não pode ser vazio."}, if: :valida?
+  validates :local_velorio, :endereco, length: {maximum: 255, message: "pode ter no máximo 255 caracteres."}, if: :valida?
+  validates :data_saida, presence: { message: "não pode ser vazio."}, if: :valida?
+  validates :hora_saida, presence: { message: "não pode ser vazio."}, length: {maximum: 5, message: "inválida."}, if: :valida?
+
+  def valida?
+    @valida ||= false
+    @valida
+  end
+
+  def valida= param
+    @valida = param
+  end
 end
