68ae9209c0cc8a113cc2d2b69701377d330cbd3b
Yara Grassi Gouffon <vaati2005-git@yahoo.com.br>
2015-06-11 12:17:38 -0300

    Campos adicionais com informações do nascimento (para o caso de nascimento e óbito ser 'sim'). Falta as outras informações (avós, etc.) [@yaragg, @erikaAkab]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150610180643) do
   create_table "cadastros", force: :cascade do |t|
     t.integer  "tipo_operacao"
