3d46d38cbda1bcf9f34f7690478bc2aaa9079eda
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-06-12 16:14:51 -0300

    Avananço na migração dos kits. [@jaodsilv, @jackiewalker]. NÃO MIGRAR!!!!.

--- db/migrate/20150611145550_update_produtos.rb
+++ db/migrate/20150611145550_update_produtos.rb
@@ -4,38 +4,42 @@ class UpdateProdutos < ActiveRecord::Migration
     create_table :urnas do |t|
       t.string :nome
       t.integer :quantidade
+      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
     create_table :revestimentos do |t|
       t.string :nome
       t.integer :quantidade
+      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
     create_table :dimensoes do |t|
-      t.reference :urnas, index: true, foreign_key: true
-      t.reference :revestimentos, index: true, foreign_key: true
+      t.references :urnas, index: true, foreign_key: true
+      t.references :revestimentos, index: true, foreign_key: true
       t.string :dimensoes
-      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
     create_table :enfeites do |t|
       t.string :nome
       t.integer :quantidade
+      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
     create_table :veus do |t|
       t.string :nome
       t.integer :quantidade
+      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
     create_table :iluminacaos do |t|
       t.string :nome
       t.integer :quantidade
+      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
@@ -72,26 +76,39 @@ class UpdateProdutos < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    create_table :outras do |t|
+    create_table :taxa_velorio do |t|
       t.boolean :enabled
-      t.decimal :valor
-      t.integer :signal
 
       t.timestamps null: false
     end
-    create_table :kits do |t|
+    create_table :mesa_de_condolencia do |t|
+      t.boolean :enabled
+
+      t.timestamps null: false
+    end
+    create_table :outras do |t|
+      t.boolean :enabled
+      t.decimal :valor
+      t.boolean :positivo
+
+      t.timestamps null: false
+    end
+    create_table :elemento_kits do |t|
       t.references :urnas, index: true, foreign_key: true
       t.references :produtos, polymorphic: true, index: true, foreign_key: true
       t.decimal :preco
 
       t.timestamps null: false
     end
-    create_table :items do |t|
+    create_table :item_compras do |t|
       t.references :kits
       t.references :compras
 
-
       t.timestamps null: false
+    end
+    change_table :compras do |t|
+      t.decimal :compras, :valor_total
+
     end
   end
 end
