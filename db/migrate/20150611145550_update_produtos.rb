0d5b7e22b16c7d4fca15704242b7baa5aee00e59
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-06-14 20:52:06 -0300

    Ultimos reparos na migração da compra. [@jaodsilv]

--- db/migrate/20150611145550_update_produtos.rb
+++ db/migrate/20150611145550_update_produtos.rb
@@ -1,114 +1,141 @@
 class UpdateProdutos < ActiveRecord::Migration
   def change
     drop_table :produtos
+    drop_table :item_compras
+
+    ##### CLASSES QUE DEVERIAM ESTAR NO MODULO DE ESTOQUE #####
+    # Urnas tem no banco do estoque e são incluídos nas compras
     create_table :urnas do |t|
       t.string :nome
-      t.integer :quantidade
-      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
+
+    # Revestimentos tem no banco do estoque e são incluídos nas compras
     create_table :revestimentos do |t|
       t.string :nome
-      t.integer :quantidade
-      t.string :unidade_de_medida
 
       t.timestamps null: false
     end
-    create_table :dimensoes do |t|
-      t.references :urnas, index: true, foreign_key: true
-      t.references :revestimentos, index: true, foreign_key: true
+
+    # Dimensões tem no banco do estoque e são incluídos nas compras
+    create_table :dimensaos do |t|
+      t.references :urna, index: true, foreign_key: true
+      t.references :revestimento, index: true, foreign_key: true
+      t.string :unidade_de_medida
       t.string :dimensoes
 
       t.timestamps null: false
     end
+
+    # Enfeites tem no banco do estoque e são incluídos nas compras
     create_table :enfeites do |t|
       t.string :nome
-      t.integer :quantidade
       t.string :unidade_de_medida
 
       t.timestamps null: false
     end
+
+    # Veus tem no banco do estoque e são incluídos nas compras
     create_table :veus do |t|
       t.string :nome
-      t.integer :quantidade
       t.string :unidade_de_medida
 
       t.timestamps null: false
     end
+
+    # Iluminação tem no banco do estoque e são incluídos nas compras
     create_table :iluminacaos do |t|
       t.string :nome
-      t.integer :quantidade
       t.string :unidade_de_medida
 
       t.timestamps null: false
     end
-    create_table :tipo_sepultamento do |t|
+
+    # Tipo sepultamento tem no banco do estoque e são incluídos nas compras
+    create_table :tipo_sepultamentos do |t|
       t.string :nome
 
       t.timestamps null: false
     end
+
+    # Carro carreto não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
     create_table :carro_carretos do |t|
       t.boolean :enabled
-      t.integer :quantidade
 
       t.timestamps null: false
     end
+
+    # Carro remoção carreto não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
     create_table :carro_remocaos do |t|
       t.boolean :enabled
-      t.integer :quantidade
 
       t.timestamps null: false
     end
+
+    # Carro enterro não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
     create_table :carro_enterros do |t|
       t.boolean :enabled
-      t.integer :quantidade
 
       t.timestamps null: false
     end
-    create_table :taxa_sepultamento do |t|
+
+    # taxa sepultamento carreto não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
+    create_table :taxa_sepultamentos do |t|
       t.boolean :enabled
 
       t.timestamps null: false
     end
-    create_table :essa_paramento do |t|
+
+    # Essa/Paramento não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
+    create_table :essa_paramentos do |t|
       t.boolean :enabled
 
       t.timestamps null: false
     end
-    create_table :taxa_velorio do |t|
+
+    # Taxa velório não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
+    create_table :taxa_velorios do |t|
       t.boolean :enabled
 
       t.timestamps null: false
     end
-    create_table :mesa_de_condolencia do |t|
+
+    # Mesa de Condolencia não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
+    # Por algum motivo rails acha que o plural de mesa condolencia é mesa condolencia
+    create_table :mesa_condolencia do |t|
       t.boolean :enabled
 
       t.timestamps null: false
     end
-    create_table :outras do |t|
-      t.boolean :enabled
-      t.decimal :valor
-      t.boolean :positivo
 
-      t.timestamps null: false
-    end
+    # Outras não tem no banco do estoque (nem está nos kits) e são incluídos nas compras,
+    # então não precisam de tabela propria
+
+    # Elemento_kits representa a tabela de preços por kit
     create_table :elemento_kits do |t|
-      t.references :urnas, index: true, foreign_key: true
-      t.references :produtos, polymorphic: true, index: true, foreign_key: true
+      t.references :urna, index: true, foreign_key: true
+      t.references :produto, polymorphic: true, index: true
       t.decimal :preco
 
       t.timestamps null: false
     end
+
+    ##### CLASSES QUE DEVERIAM ESTAR AQUI MESMO #####
+    # Item_compras representa um item de uma compra
     create_table :item_compras do |t|
-      t.references :kits
-      t.references :compras
+      t.string :nome
+      t.boolean :enabled
+      t.string :unidade_de_medida
+      t.string :dimensoes
+      t.string :tipo
+      t.integer :quantidade
+      t.decimal :preco
+      t.boolean :sinal
+
+      t.references :compra, index: true, foreign_key: true
 
       t.timestamps null: false
-    end
-    change_table :compras do |t|
-      t.decimal :compras, :valor_total
-
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,82 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+urnas = Urna.create([{nome: "Urna Fixa", dimensaos: []},{nome: "Bromélia", dimensaos: []}])
+revestimentos = Revestimento.create([{nome: "Revestimento Fixo", dimensaos: []},{nome: "Mármore", dimensaos: []}])
+
+# dimensoes
+dimensoes = ["2,00x0,60x0,40", "1,90x0,60x0,40"]
+urna = urnas.first
+urna.dimensaos.append(Dimensao.new({dimensoes: dimensoes.first, unidade_de_medida: 'm'}))
+urna.dimensaos.append(Dimensao.new({dimensoes: dimensoes.last, unidade_de_medida: 'm'}))
+urna.save
+urna = urnas.last
+urna.dimensaos.append(Dimensao.new({dimensoes: dimensoes.first, unidade_de_medida: 'm'}))
+urna.dimensaos.append(Dimensao.new({dimensoes: dimensoes.last, unidade_de_medida: 'm'}))
+urna.save
+
+revestimento = revestimentos.first
+revestimento.dimensaos.append(Dimensao.new({dimensoes: dimensoes.first, unidade_de_medida: 'm'}))
+revestimento.dimensaos.append(Dimensao.new({dimensoes: dimensoes.last, unidade_de_medida: 'm'}))
+revestimento.save
+revestimento = revestimentos.last
+revestimento.dimensaos.append(Dimensao.new({dimensoes: dimensoes.first, unidade_de_medida: 'm'}))
+revestimento.dimensaos.append(Dimensao.new({dimensoes: dimensoes.last, unidade_de_medida: 'm'}))
+revestimento.save
+
+enfeites = Enfeite.create([{nome: "Véu", unidade_de_medida: "pacote"},{nome: "Flores", unidade_de_medida: "Pacote"}])
+veus = Veu.create([{nome: "Rendado", unidade_de_medida: "Unidade"}, {nome: "Liso", unidade_de_medida: "Unidade"}])
+iluminacaos = Iluminacao.create([{nome: "Velas", unidade_de_medida: "Pacote"}, {nome: "Fluorescente", unidade_de_medida: "Lâmpada"}])
+tipo_sepultamento = TipoSepultamento.create({nome: "Qd. Geral Terra s/ Gaveta"})
+carro_carreto = CarroCarreto.create({enabled: true})
+carro_remocao = CarroRemocao.create({enabled: true})
+carro_enterro = CarroEnterro.create({enabled: true})
+taxa_sepultamento = TaxaSepultamento.create({enabled: true})    # taxa sepultamento carreto não tem no banco do estoque (mas está nos kits) e são incluídos nas compras
+essa_paramento = EssaParamento.create({enabled: true})
+taxa_velorio = TaxaVelorio.create({enabled: true})
+mesa_condolencia = MesaCondolencia.create({enabled: true})
+
+def add_produto urna, produto, preco
+  elemento_kit = ElementoKit.new({preco: preco})
+  elemento_kit.urna = urna
+  elemento_kit.produto = produto
+end
+
+# Kit Urna Fixa
+urna = urnas.first
+add_produto urna, revestimentos.first, 1.99
+add_produto urna, revestimentos.last, 2.99
+add_produto urna, enfeites.first, 3.99
+add_produto urna, enfeites.last, 4.99
+add_produto urna, veus.first, 5.99
+add_produto urna, veus.last, 6.99
+add_produto urna, iluminacaos.first, 7.99
+add_produto urna, iluminacaos.last, 8.99
+add_produto urna, tipo_sepultamento, 9.99
+add_produto urna, carro_carreto, 10.99
+add_produto urna, carro_remocao, 10.99
+add_produto urna, carro_enterro, 10.99
+add_produto urna, taxa_sepultamento, 10.99
+add_produto urna, essa_paramento, 10.99
+add_produto urna, taxa_velorio, 10.99
+add_produto urna, mesa_condolencia, 10.99
+
+# Kit Bromélia
+urna = urnas.second
+add_produto urna, revestimentos.first, 1.99
+add_produto urna, revestimentos.last, 2.99
+add_produto urna, enfeites.first, 3.99
+add_produto urna, enfeites.last, 4.99
+add_produto urna, veus.first, 5.99
+add_produto urna, veus.last, 6.99
+add_produto urna, iluminacaos.first, 7.99
+add_produto urna, iluminacaos.last, 8.99
+add_produto urna, tipo_sepultamento, 9.99
+add_produto urna, carro_carreto, 10.99
+add_produto urna, carro_remocao, 10.99
+add_produto urna, carro_enterro, 10.99
+add_produto urna, taxa_sepultamento, 10.99
+add_produto urna, essa_paramento, 10.99
+add_produto urna, taxa_velorio, 10.99
+add_produto urna, mesa_condolencia, 10.99
