c7a94ed5a89490b37f2aab05b49bf84ccf53a6e7
João M. M. da Silva <jaodsilv@linux.ime.usp.br>
2015-06-14 21:35:12 -0300

    Nome do fluxo correto. [@jaodsilv]

--- app/models/cadastro.rb
+++ app/models/cadastro.rb
@@ -22,4 +22,30 @@ class Cadastro < ActiveRecord::Base
       "Processo Gratuito Desconhecido (GD)"
     end
   end
+
+  def nome_fluxo
+    case tipo_operacao
+    when 1
+      case tipo_contratacao
+      when 1
+        case destino_final
+        when 1
+          "Processo Pago Normal Sepultamento (PNS)"
+        else
+          ""
+        end
+      else
+        ""
+      end
+    when 2
+      case tipo_contratacao
+      when 2
+        "Processo Gratuito Desconhecido (GD)"
+      else
+        ""
+      end
+    else
+      ""
+    end
+  end
 end
