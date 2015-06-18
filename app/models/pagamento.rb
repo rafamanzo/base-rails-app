f344be898b278c230a80cdc316e22e4820a76f55
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2015-06-18 15:21:13 -0300

    Fix no model pagamento, deveria ser has_many :info_pagamento.

--- app/models/pagamento.rb
+++ app/models/pagamento.rb
@@ -1,6 +1,6 @@
 class Pagamento < ActiveRecord::Base
   belongs_to :cadastro
-  has_one :info_pagamento
+  has_many :info_pagamento
 
   accepts_nested_attributes_for :info_pagamento
   validates_associated :info_pagamento
