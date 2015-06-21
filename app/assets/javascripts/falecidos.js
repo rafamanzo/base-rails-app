666c7f961f25ac9492dd254911617b549a39854f
suelengc <suelengcarvalho@gmail.com>
2015-06-21 10:46:14 -0300

    Na tela do falecido, ao preencher a data de nascimento, o campo idade é preenchido automaticamente [@suelengc]

--- app/assets/javascripts/falecidos.js
+++ app/assets/javascripts/falecidos.js
@@ -1,2 +1,28 @@
-$("#data_nascimento").datepicker({ dateFormat: 'dd/mm/yyyy' });
+function calculaIdade(d) {
+	var date = new Date(d)
+	
+	year = date.getFullYear();
+    month = date.getMonth() + 1;
+    day = date.getDate();
 
+    $("#idade").val(_calculaIdade(year, month, day));
+}
+
+function _calculaIdade(ano_aniversario, mes_aniversario, dia_aniversario) {
+    var d = new Date,
+    ano_atual = d.getFullYear(),
+    mes_atual = d.getMonth() + 1,
+    dia_atual = d.getDate(),
+
+    ano_aniversario = +ano_aniversario,
+    mes_aniversario = +mes_aniversario,
+    dia_aniversario = +dia_aniversario,
+
+    quantos_anos = ano_atual - ano_aniversario;
+
+    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--;
+    }
+
+    return quantos_anos < 0 ? 0 : quantos_anos;
+}
